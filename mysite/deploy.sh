c1aa76fdd4b21ccd150ef5d19a4f7a651edb07a5
YumaKaminaga <kaminaga@nebit.co.jp>
2018-06-14 17:11:55 +0900

    taskにrefresh_from_dbの追加
    * 長時間非同期処理であるtaskにrefresh_from_dbの追加
        * 既知のバグの更新が上書きされる現象の改善
    * deployスクリプト等の追加

---
+++ mysite/deploy.sh
@@ -0,0 +1,30 @@
+# deploy commands
+
+# git pull
+echo "git pull..."
+git pull
+
+# migrate
+echo "making migrations..."
+python manage.py makemigrations
+echo "apply migrations..."
+python manage.py migrate
+
+# collect static
+echo "collect statics..."
+python manage.py collectstatic
+
+# kill crun
+echo "stop celery..."
+./stopcrun.sh
+
+# restart apache
+echo "restart apachce"
+sudo apache2ctl restart
+
+# start crun
+echo "starting celery..."
+./crun.sh &
+echo "starting celeryBeats..."
+./crun.sh &
+./cbeat.sh &
